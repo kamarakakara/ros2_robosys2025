5f948f4dcf8795c940c379fbe5e52312ed952317
kakara <s22c1701ms@s.chibakoudai.jp>
2025-12-29 02:34:03 +0900

    add test

---
+++ test/test.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch ros2_robosys2025 talk_listen.launch.py > /tmp/ros2_robosys2025.log |
+grep 'Listen: 10'
