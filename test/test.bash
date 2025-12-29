7972eeef976f1045d70f532fc0e8776d5fe6411f
kakara <s22c1701ms@s.chibakoudai.jp>
2025-12-30 02:22:02 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -6,5 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch ros2_robosys2025 talk_listen.launch.py > /tmp/ros2_robosys2025.log |
+timeout 10 ros2 launch ros2_robosys2025 talk_listen.launch.py > /tmp/ros2_robosys2025.log
+
+cat /tmp/ros2_robosys2025.log |
 grep 'Listen: 10'
