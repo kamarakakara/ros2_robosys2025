f171d1b7b91405237b8ad00326973b9896e7d75d
kakara <s22c1701ms@s.chibakoudai.jp>
2026-01-14 02:32:05 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -7,38 +7,40 @@ cd "$dir/ros2_ws" || exit 1
 colcon build || exit 1
 source "$dir/.bashrc"
 
+LOG_FILE=/tmp/ros2_robosys2025.log
+> "$LOG_FILE"
+
 
 # 自動更新
-timeout 40 ros2 launch ros2_robosys2025 talk_listen.launch.py \
-  > /tmp/ros2_robosys2025.log
+timeout 40 ros2 launch ros2_robosys2025 talk_listen.launch.py > "$LOG_FILE"
 
-count=$(tail -n +5 /tmp/ros2_robosys2025.log \
-| grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
-| wc -l)
+count_auto=$(tail -n +5 "$LOG_FILE" \
+  | grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
+  | wc -l)
 
 
 # 80を上回る、下回る
-ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 50.00}"
-
-ros2 run my_package listener &  # バックグラウンドで起動
+ros2 run my_package listener &
 LISTENER_PID=$!
-sleep 5
+sleep 2
 
+ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 50.00}"
+sleep 5
 ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 90}"
 sleep 5
-
 ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 50.00}"
-
-
 sleep 2
+
 kill $LISTENER_PID 2>/dev/null
 
+count_listener=$(tail -n +2 "$LOG_FILE" \
+  | grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
+  | wc -l)
 
+# 合計
+count=$((count_auto + count_listener))
 
-total_count=$(grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' /tmp/ros2_robosys2025.log | wc -l)
-
-
-if [ "$total_count" -ge 5 ]; then
+if [ "$count" -eq 5 ]; then
   exit 0
 else
   exit 1
