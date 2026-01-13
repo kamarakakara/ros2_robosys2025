bf4911992202823abcb15f805240e30333cb7887
kakara <s22c1701ms@s.chibakoudai.jp>
2026-01-14 01:19:03 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -10,10 +10,13 @@ source "$dir/.bashrc"
 timeout 40 ros2 launch ros2_robosys2025 talk_listen.launch.py \
   > /tmp/ros2_robosys2025.log
 
-tail -n +5 /tmp/ros2_robosys2025.log \
+count=$(tail -n +5 /tmp/ros2_robosys2025.log \
 | grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
-| uniq -c \
-| awk '$1 >= 2 { exit 0 } END { exit 1 }'
+| wc -l)
 
-exit $?
+if [ "$count" -ge 2 ]; then
+  exit 0
+else
+  exit 1
+fi
 
