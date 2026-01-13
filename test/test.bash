86cab2273f400a40d74b2d51d68080fac126b583
kakara <s22c1701ms@s.chibakoudai.jp>
2026-01-14 00:55:43 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -3,21 +3,17 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws || exit 1
+cd "$dir/ros2_ws" || exit 1
 colcon build || exit 1
-source $dir/.bashrc
+source "$dir/.bashrc"
 
-timeout 120 ros2 launch ros2_robosys2025 talk_listen.launch.py \
+timeout 10 ros2 launch ros2_robosys2025 talk_listen.launch.py \
   > /tmp/ros2_robosys2025.log
 
 tail -n +5 /tmp/ros2_robosys2025.log \
-| grep '[0-9]' \
+| grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
 | uniq -c \
 | awk '$1 >= 5 { exit 0 } END { exit 1 }'
 
-if [ $? -eq 0 ]; then
-  exit 0
-else
-  exit 1
-fi
+exit $?
 
