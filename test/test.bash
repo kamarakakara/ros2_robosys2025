80198e2bdce1709955f4e954ebbea8922f1589aa
kakara <s22c1701ms@s.chibakoudai.jp>
2026-01-14 01:46:37 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -7,6 +7,8 @@ cd "$dir/ros2_ws" || exit 1
 colcon build || exit 1
 source "$dir/.bashrc"
 
+
+# 自動更新
 timeout 40 ros2 launch ros2_robosys2025 talk_listen.launch.py \
   > /tmp/ros2_robosys2025.log
 
@@ -14,7 +16,29 @@ count=$(tail -n +5 /tmp/ros2_robosys2025.log \
 | grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' \
 | wc -l)
 
-if [ "$count" -ge 2 ]; then
+
+# 80を上回る、下回る
+ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 50.00}"
+
+ros2 run my_package listener &  # バックグラウンドで起動
+LISTENER_PID=$!
+sleep 5
+
+ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 90}"
+sleep 5
+
+ros2 topic pub --once /rum_usage std_msgs/msg/Float32 "{data: 50.00}"
+
+
+sleep 2
+kill $LISTENER_PID 2>/dev/null
+
+
+
+total_count=$(grep -E '^\[listener-[0-9]+\][[:space:]]+(0\.[0-9]+|[1-9][0-9]*(\.[0-9]+)?)$' /tmp/ros2_robosys2025.log | wc -l)
+
+
+if [ "$total_count" -ge 5 ]; then
   exit 0
 else
   exit 1
