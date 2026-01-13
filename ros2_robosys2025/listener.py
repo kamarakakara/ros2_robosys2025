d50c2bf40a5173b81cdcd5f4b72dd0d07db5d086
kakara <s22c1701ms@s.chibakoudai.jp>
2026-01-14 00:04:45 +0900

    fix

--- ros2_robosys2025/listener.py
+++ ros2_robosys2025/listener.py
@@ -16,11 +16,11 @@ def callback(msg):
     latest_usage = msg.data
 
     if latest_usage > WARNING_THRESHOLD and not is_warning:
-        print("Usage rate exceeds 80%", flush=True)
+        print("{latest_usage:.2f}", flush=True)
         is_warning = True
 
     elif latest_usage <= WARNING_THRESHOLD and is_warning:
-        print(f"Usage rate below 80%", flush=True)
+        print(f"{latest_usage:.2f}", flush=True)
         is_warning = False
 
 def main():
@@ -45,13 +45,13 @@ def main():
 
             # 起動時出力
             if latest_usage is not None and not has_printed_initial:
-                print(f"Current usage : {latest_usage:.2f}%", flush=True)
+                print(f"{latest_usage:.2f}", flush=True)
                 has_printed_initial = True
                 last_print_time = now
 
             # 30秒ごとの定期出力
             elif latest_usage is not None and (now - last_print_time) >= 30.0:
-                print(f"Current usage : {latest_usage:.2f}%", flush=True)
+                print(f"{latest_usage:.2f}", flush=True)
                 last_print_time = now
 
     except KeyboardInterrupt:
